cd9c49e04af6fdfc2ce1ffc13e20767579dd1d90
Biruk Ephrem <birukeph@gmail.com>
2022-07-09 19:46:37 +0300

    hidden back to top button

--- src/components/BackToTop.vue
+++ src/components/BackToTop.vue
@@ -1,4 +1,12 @@
 <template>
-	<div id="back-to-top">
+	<div id="back-to-top" class="
+		fixed right-16 bottom-16 cursor-pointer hidden
+	">
+		<img src="../assets/icons/down-arrow-3.svg" class="
+			2xl:h-[2.9rem]
+			md:h-[2.25rem]
+			h-[1.2rem]
+			rotate-180
+		"/>
 	</div>
 </template>
